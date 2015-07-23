fffedad8a916bd1af39b906f4c2137c6f7d26921
kwangchun Lee <kwangchun.lee.7@gmail.com>
2015-07-23 13:39:31 +0900

    한글 번역을 위한 초기 설정 .

--- ch_intro_to_data/figures/variables/variables.R
+++ ch_intro_to_data/figures/variables/variables.R
@@ -1,41 +1,49 @@
 library(openintro)
 data(COL)
 
-myPDF('variables.pdf', 4.2, 1.5, mar = rep(0,4))
+library(showtext)
+font.add('나눔고딕코딩', 'NanumGothicCoding.ttf') ## add font
+
+#myPDF('variables.pdf', 4.2, 1.5, mar = rep(0,4))
+myPDF('variables.pdf', 4.2, 1.5, mar = rep(0,4), family = '나눔고딕코딩')
+#cairo_pdf('variables.pdf', family = '나눔고딕코딩')
 plot(c(-0.15, 1.3),
      c(0, 1),
      type = 'n',
      axes = FALSE)
 
-text(0.6, 0.9, 'all variables')
+text(0.6, 0.9, '전체 변수', family='NanumGothicCoding')
 rect(0.4, 0.8, 0.8, 1)
 
-text(0.25, 0.5, 'numerical')
+text(0.25, 0.5, '숫자형')
 rect(0.1, 0.4, 0.4, 0.6)
 arrows(0.45, 0.78, 0.34, 0.62, length = 0.08)
 
-text(0.9, 0.5, 'categorical')
+text(0.9, 0.5, '범주형')
 rect(0.73, 0.4, 1.07, 0.6)
 arrows(0.76, 0.78, 0.85, 0.62, length = 0.08)
 
-text(0, 0.1, 'continuous')
+text(0, 0.1, '연속형')
 rect(-0.17, 0, 0.17, 0.2)
 arrows(0.13, 0.38, 0.05, 0.22, length = 0.08)
 
-text(0.39, 0.1, 'discrete')
+text(0.39, 0.1, '이산형')
 rect(0.25, 0, 0.53, 0.2)
 arrows(0.35, 0.38, 0.4, 0.22, length = 0.08)
 
-text(0.77, 0.14, 'nominal', col = COL[6], cex = 0.7)
-text(0.77, 0.05, '(unordered categorical)',
+text(0.77, 0.14, '명목형', col = COL[6], cex = 0.7)
+text(0.77, 0.05, '(순서없는 범주형)',
      col = COL[6],
      cex = 0.5)
 rect(0.6, 0, 0.94, 0.2, border = COL[6])
 arrows(0.82, 0.38, 0.77, 0.22, length = 0.08, col = COL[6])
 
-text(1.14, 0.14, 'ordinal', col = COL[6], cex = 0.7)
-text(1.14, 0.05, '(ordered categorical)', col = COL[6], cex = 0.5)
+text(1.14, 0.14, '서수형', col = COL[6], cex = 0.7)
+text(1.14, 0.05, '(순서있는 범주형)', col = COL[6], cex = 0.5)
 rect(0.98, 0, 1.3, 0.2, border = COL[6])
 arrows(1.03, 0.38, 1.11, 0.22, length = 0.08, col = COL[6])
 
 dev.off()
+
+
+
